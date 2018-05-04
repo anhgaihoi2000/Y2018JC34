ceaec3423f31e506f4926b1b3a2a6e3bfdc9921f
anhgaihoi2000 <quannmph04929@fpt.edu.vn>
2018-05-04 20:31:23 +0700

    quan

--- Y2018JC34/src/com/imic/students/NguyenMinhQuan_Buoi5/Bai4.java
+++ Y2018JC34/src/com/imic/students/NguyenMinhQuan_Buoi5/Bai4.java
@@ -8,60 +8,64 @@ public class Bai4 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
-		String hoten = new String();
+		
 		System.out.println("Ban nhap 4 ho ten hoc sinh:");
-		String a[] = new String[3];
-		hoten = sc.nextLine();
+		String []hoten = new String[4];
+		
 	
 
-		for (int i = 0; i < 3; i++) {
-			a[i] = sc.nextLine();
+		for (int i = 0; i < 4; i++) {
+			hoten[i] =sc.nextLine();
+			
 		}
 
-		double diem = 0;
-		System.out.println("Moi ban nhap diem 4 hoc sinh:");
-		double b[]= new double[3];
-		diem = sc.nextDouble();
 		
-		for (int i = 0; i < 3; i++) {
+		System.out.println("Moi ban nhap diem 4 hoc sinh:");
+		double []diem= new double[4];
+		
+		
+		
+		for (int i = 0; i < 4; i++) {
+			diem[i] =sc.nextDouble();
 			
-			b[i] = sc.nextDouble();
+			
 		}
 		
 		String hocluc = new String();
 
-		for (int t = 0; t < 3; t++) {
+		for (int t = 0; t < 4; t++) {
 
-			if (b[t] < 5) {
-				System.out.println("Ban: " + a[t] + "----- Hoc luc: Yeu");
+			if (diem[t] < 5) {
+				System.out.println("Ban: " + hoten[t] + "----- Hoc luc: Yeu");
 
-			} else if (5 <= b[t] && b[t] < 6.5) {
-				System.out.println("Ban: " + a[t] + "----- Hoc luc: Trung binh");
+			} else if (5 <= diem[t] && diem[t] < 6.5) {
+				System.out.println("Ban: " + hoten[t] + "----- Hoc luc: Trung binh");
 
-			} else if (6.5 <= b[t] && b[t] < 7.5) {
-				System.out.println("Ban: " + a[t] + "----- Hoc luc: Kha");
+			} else if (6.5 <= diem[t] && diem[t] < 7.5) {
+				System.out.println("Ban: " +hoten[t] + "----- Hoc luc: Kha");
 
-			} else if (7.5 <= b[t] && b[t] < 9) {
-				System.out.println("Ban: " + a[t] + "----- Hoc luc: Gioi");
+			} else if (7.5 <= diem[t] && diem[t] < 9) {
+				System.out.println("Ban: " +hoten[t] + "----- Hoc luc: Gioi");
 
 			} else {
-				System.out.println("Ban: " + a[t] + "----- Hoc luc: Xuat sac");
+				System.out.println("Ban: " + hoten[t] + "----- Hoc luc: Xuat sac");
 
 			}
 		}
-		for (int d = 0; d < 3 - 1; d++) {
-			for (int e = d + 1; e < 3; e++) {
-				if (b[d] > b[e]) {
-					double temp = b[d];
-					b[d] = b[e];
-					b[d] = temp;
-
-				}
-			}
-		}
-		System.out.print("Sap xep diem theo thu tu tang dan  : " + Arrays.toString(b));
-
-	}
-
-}
+	}}
+//		for (int d = 0; d < 3 - 1; d++) {
+//			for (int e = d + 1; e < 3; e++) {
+//				if (b[d] > b[e]) {
+//					double temp = b[d];
+//					b[d] = b[e];
+//					b[d] = temp;
+//
+//				}
+//			}
+//		}
+//		System.out.print("Sap xep diem theo thu tu tang dan  : " + Arrays.toString(b));
+//
+//	}
+//
+//}
 //aa//
